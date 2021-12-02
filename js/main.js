dfbac2a5964ef71709c0db081069e9f60b80a6c5
123ahmed565 <ahmedseragmohamed@gmail.com>
2021-12-02 11:09:45 +0200

    [DEV] add modal popup search for mobile & edite in picture from cover to be contain

--- js/main.js
+++ js/main.js
@@ -118,4 +118,22 @@ $(document).on('click','.deleted',function(){
     $('.delete-box'+"."+id).remove();
 });
 
+// modal search popup for mobile
+$('a[href="#search"]').click(function() {
+    event.preventDefault()
+    $("#search-box").addClass("-open");
+    setTimeout(function() {
+    inputSearch.focus();
+    }, 800);
+});
 
+$('a[href="#close"]').click(function() {
+    event.preventDefault()
+    $("#search-box").removeClass("-open");
+});
+
+$(document).keyup(function(e) {
+    if (e.keyCode == 27) { // escape key maps to keycode `27`
+    $("#search-box").removeClass("-open");
+    }
+});
