c5822283095441ae075c8ad5d2c84f6f5eb5bc8c
123ahmed565 <ahmedseragmohamed@gmail.com>
2021-12-05 11:06:52 +0200

    [DEV] update in photo and footer & addd preloader & top button

--- js/main.js
+++ js/main.js
@@ -137,3 +137,29 @@ $(document).keyup(function(e) {
     $("#search-box").removeClass("-open");
     }
 });
+
+
+// to top
+const scrollBtn= document.querySelector(".scroll-to-top");
+const upperbar =document.querySelector('.upperbar');
+
+scrollBtn.style.display="none";
+
+scrollBtn.addEventListener("click",()=>{
+document.body.scrollTop=0;
+document.documentElement.scrollTop=0;
+});
+
+document.addEventListener("scroll",(e)=>{
+if(document.documentElement.scrollTop <=100){
+scrollBtn.style.display="none";
+}else
+{
+scrollBtn.style.display="block";
+}
+});
+
+// preloader
+window.onload = function () {
+    document.querySelector(".preloaders").style.display = "none";
+};
