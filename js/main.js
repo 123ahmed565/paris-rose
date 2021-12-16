efa68cb227b739dce2d29df3d7a50d5a286647ec
123ahmed565 <ahmedseragmohamed@gmail.com>
2021-12-16 12:00:24 +0200

    [DEV] add soial login fb&google to lofin system

--- js/main.js
+++ js/main.js
@@ -220,6 +220,10 @@ var swiper = new Swiper(".mySwiper3", {
     el: ".swiper-pagination",
     clickable: true,
     },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
     breakpoints: {
         0: {
             slidesPerView: 1,
